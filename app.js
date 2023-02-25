146957feb826ecd2af1947ace160aed21f23f295
azizbekumarzhonov <aziz22052007@gmail.com>
2023-02-25 19:18:44 +0300

    new

---
+++ app.js
@@ -0,0 +1,8 @@
+const drums = document.querySelector(".drum");
+
+drums.addEventListener("click", () => {
+    const pressed = new Audio("./002 MURIAM - ENTA EL HAYAT.mp3");
+    pressed.play();
+})
+
+
